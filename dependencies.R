66251e5d0092d2d9792fbf000fa7471c57fd7ded
cowvin0 <gabrielpereira00PereiraGabriel@outlook.com>
2023-11-16 14:21:32 -0300

    adicionando docker para aplicação e servidor R

--- dependencies.R
+++ dependencies.R
@@ -1,2 +1,8 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
 library(rhino)
+library(dplyr)
+library(tidyr)
+library(shiny)
+library(stringr)
+library(vroom)
+library(jsonlite)
